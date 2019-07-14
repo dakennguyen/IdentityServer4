5f348757106e2e0220b0a583d9be5aa5a1fecf87
Khoa Nguyen <ngdoan.anhkhoa@gmail.com>
2019-07-14 18:52:38 +0700

    Api: IdentityController

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -15,6 +15,18 @@ namespace Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+
+                    options.Audience = "api1";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +36,9 @@ namespace Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseAuthentication();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
